baf28fd1187f05617ccbbcb500870bac7bef3e07
Khoa Le <khoa.le@tuni.fi>
2025-07-22 20:13:34 +0700

    Dependencies for heart rate logging

---
+++ scripts/heartrate_log.py
@@ -0,0 +1,8 @@
+import cv2
+import traceback
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from retinaface.pulse_retina import PulseMonitor
+import csv
+import time
